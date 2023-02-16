d68adbc8e36e83faaf45f618fde507c93f54d811
DDJM-77 <ddjm77340@gmail.com>
2023-02-16 11:08:35 +0100

    Completed with two remaining regressions

--- chap-04/1-hierarchy/Staff.cpp
+++ chap-04/1-hierarchy/Staff.cpp
@@ -1,18 +1,12 @@
 #include "Staff.h"
 
+#include "Rank.h"
+
 #include <cstdio>
 
 Staff::Staff(std::string_view name, std::string_view surname, unsigned int salary)
-    : _name { name }
-    , _surname { surname }
-    , _salary { salary }
+    : Employee { name, surname, salary, Rank::Default }
 {}
-
-void Staff::increase_salary(unsigned int raise)
-{
-    _salary += raise;
-}
-
 void Staff::receive_task()
 {
     std::printf("%s %s (Staff): \"No problem! I'm on it!\"\n", get_name().c_str(), get_surname().c_str());
--- chap-04/2-farm/FarmHouse.cpp
+++ chap-04/2-farm/FarmHouse.cpp
@@ -3,13 +3,31 @@
 #include "Chicken.h"
 #include "Cow.h"
 #include "Dog.h"
+#include "Opera.hpp"
+
+#include <vector>
 
 void sing_a_lot(const Animal& animal, unsigned int times)
 {
-    while (times-- > 0)
+    if (times > 0)
     {
-        animal.sing();
+        while (--times > 0)
+        {
+            animal.sing(' ');
+        }
+
+        animal.sing('\n');
     }
+}
+
+void sing_chorus(const std::vector<Animal*>& animals)
+{
+    for (const auto& animal : animals)
+    {
+        animal->sing(' ');
+    }
+
+    std::cout << std::endl;
 }
 
 int main()
@@ -19,11 +37,22 @@ int main()
     Dog     dog;
     Cow     cow;
 
+    std::vector<Animal*> animals;
+
     sing_a_lot(cat, 2u);
     sing_a_lot(dog, 1u);
     sing_a_lot(cow, 3u);
     sing_a_lot(dog, 1u);
     sing_a_lot(chicken, 2u);
+
+    animals.push_back(&cat);
+    animals.push_back(&dog);
+    animals.push_back(&cow);
+
+    sing_chorus(animals);
+
+    Opera opera;
+    opera.sing();
 
     return 0;
 }
